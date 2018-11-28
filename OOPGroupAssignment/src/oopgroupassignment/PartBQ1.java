d37834b6b37f0a798f632bff5f098e2648d4aa56
Shafiq Aiman <32039212+ShafiqAiman@users.noreply.github.com>
2018-11-28 10:53:15 +0800

    Update PartBQ1.java

--- OOPGroupAssignment/src/oopgroupassignment/PartBQ1.java
+++ OOPGroupAssignment/src/oopgroupassignment/PartBQ1.java
@@ -6,6 +6,8 @@
 package oopgroupassignment;
 
 import javax.swing.JOptionPane;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  *
@@ -18,20 +20,36 @@ public class PartBQ1 {
         double Product1 = 0;
         double Product3 = 0;
         double Product4 = 0;
+        String membershipstatus = "";   
+        double rebate = 0;
         JOptionPane.showMessageDialog(null,"Welcome to UNIMY Online Store!","Output",JOptionPane.INFORMATION_MESSAGE);
-        String a = JOptionPane.showInputDialog(null,"Do you have membership card? \nA. Yes\nB. No","Membership",JOptionPane.QUESTION_MESSAGE);
-        char membership = a.charAt(0);
+        
         double discount = 0;
+        
+        int counter =1;
+        
+        while(counter > 0){
+            
+        String a = JOptionPane.showInputDialog(null,"Do you have membership card? \nA. Yes\nB. No","Membership",JOptionPane.QUESTION_MESSAGE);
+        char membership = a.charAt(0);    
+        
+            
         if((membership == 'A')||(membership == 'a')){
             discount = 0.1;
-            String membershipstatus = "Yes";
-        }else{
+            counter = 0;
+            membershipstatus= "Yes";
+        }else if((membership == 'B') || (membership == 'b')){
             discount = 0;
-            String membershipstatus = "No";
+            counter = 0;
+            membershipstatus = "No";
+        }else{
+            JOptionPane.showMessageDialog(null,"Please enter A or B only !","Membership",JOptionPane.WARNING_MESSAGE);
+            counter++;
+        }
         }
         String b = JOptionPane.showInputDialog(null,"How many products that you want to purchase?","Membership",JOptionPane.QUESTION_MESSAGE);
         int NoProduct = Integer.parseInt(b);
-        int ProductDiscount = 0;
+        //int ProductDiscount = 0;
         
         while(NoProduct>0){
             String ProductCode = JOptionPane.showInputDialog(null,"Please enter product code :","Product Code",JOptionPane.QUESTION_MESSAGE);
@@ -82,14 +100,16 @@ public class PartBQ1 {
             String d = JOptionPane.showInputDialog(null,"Please enter the quantity of product that you want to purchase:","Membership",JOptionPane.QUESTION_MESSAGE);
             int ProductQuantity = Integer.parseInt(d);
             
+            /*
             if(ProductCode == "1"){
-                Product1 += (price*ProductQuantity);
+                Product1 = (price*ProductQuantity);
             }else if(ProductCode == "3"){
-                Product3 += (price*ProductQuantity);
+                Product3 = (price*ProductQuantity);
             }else if(ProductCode == "4"){
-                Product4 += (price*ProductQuantity);
+                Product4 = (price*ProductQuantity);
+                System.out.println(Product4);
             }
-            
+//          */
             
             
             NoProduct -= ProductQuantity;
@@ -98,7 +118,7 @@ public class PartBQ1 {
             totalquantity += ProductQuantity;
         }
         
-        double rebate = 0;
+        
         if(Product1 > 200)
             rebate += 25;
         if(Product3 > 200)
@@ -110,38 +130,62 @@ public class PartBQ1 {
             
         
         
-        double TotalPrice = totalprice - (totalprice*discount);
+        //double TotalPrice = totalprice - (totalprice*discount);
         
         double discount1 = 0;
-        if((TotalPrice>200)&&(TotalPrice<=500)){
+        if((totalprice>200)&&(totalprice<=500)){
             discount1 = 0.1;
-        }else if((TotalPrice > 500)&&(TotalPrice<=1000)){
+        }else if((totalprice > 500)&&(totalprice<=1000)){
             discount1 = 0.2;
-        }else if((TotalPrice > 1000)&&(TotalPrice<=2000)){
+        }else if((totalprice > 1000)&&(totalprice<=2000)){
             discount1 = 0.3;
-        }else if(TotalPrice > 2000){
+        }else if(totalprice > 2000){
             discount1 = 0.5;
         }else{
             discount1 = 0;
         }
         
+        
+        //System.out.println("discount1 = " + discount1);
+        /*
         if (rebate == 0){
-            TotalPrice = TotalPrice - (TotalPrice*discount1);
+            TotalPrice = TotalPrice - 0;
         }else{
-            TotalPrice = TotalPrice - (TotalPrice*discount1) - rebate;
+            TotalPrice = TotalPrice - rebate;
         }
+        */
         
-        
-        
+        double TotalPrice = totalprice - (totalprice*discount);
+        //System.out.println("TotalPrice = "+TotalPrice);
+        TotalPrice = TotalPrice - (TotalPrice*discount1);
+        //System.out.println("TotalPrice = "+TotalPrice);
         TotalPrice = TotalPrice + (TotalPrice*0.06);
         
-        
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("\n\nyyyy/MM/dd HH:mm:ss");
+	LocalDateTime now = LocalDateTime.now();
+	System.out.println(dtf.format(now)); //2016/11/16 12:08:43
         System.out.print("------------------------------UNIMY ONLINE STORE-----------------------------\n");
         System.out.print("Code\t\tName\t\tPrice\t\tQuantity\t\tTotal\n\n\n");
-        //System.out.println("Membership status = "+membershipstatus);
-        System.out.println("total price = "+totalprice);
-        System.out.println("actual total price = "+TotalPrice);
-        System.out.println("Total Quantity of purchased products = "+totalquantity);
+        System.out.print("-----------------------------------------------------------------------------\n");
         
+        System.out.println("Total items quantity = "+totalquantity);
+        System.out.println("Membership status = " + membershipstatus);
+        
+        if(membershipstatus == "Yes"){
+            System.out.println("Membership Discount = 10%");
+        }else if(membershipstatus == "No"){
+            System.out.println("Membership Discount = 0%");
+        }
+        
+        System.out.printf("Total Purchased = RM %.2f \n",totalprice);
+        //System.out.println("Rebate = RM "+rebate);
+        System.out.println("Discount = " + discount1*100 + "%");
+        System.out.println("Government Service Tax = 6%");
+        System.out.print("-----------------------------------------------------------------------------\n");
+        System.out.printf("Net Purchased = RM %.2f \n",TotalPrice);
+        //amount paid
+        //balance
+        
+        System.out.print("-----------------------------------------------------------------------------\n");
     }
 }
